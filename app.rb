e2aaa5123b3cad4ef6b0170678eb908276241977
ktn0817 <kashif.atcha@hotmail.co.uk>
2022-01-17 15:49:38 +0000

    bookmark class and feature test created

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   configure :development do
@@ -8,6 +9,16 @@ class BookmarkManager < Sinatra::Base
 
   get '/' do
     'Bookmark Manager'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = [
+      "http://www.makersacademy.com",
+      "http://www.destroyallsoftware.com",
+      "http://www.google.com"
+    ]
+
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $0
