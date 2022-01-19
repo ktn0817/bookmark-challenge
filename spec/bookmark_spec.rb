47ff0a6b6d05ad6cbedf3083a87ee82ae1c66722
ktn0817 <kashif.atcha@hotmail.co.uk>
2022-01-19 16:32:21 +0000

    connected to database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -10,4 +10,14 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.google.com")
     end
   end
+
+  describe '.all' do
+    it 'returns a list of bookmarks' do
+      bookmarks = Bookmark.all
+
+      expect(bookmarks).to include "http://www.makersacademy.com"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
+      expect(bookmarks).to include "http://www.google.com"
+    end
+  end
 end
